49a34f81003a75bbcb0e8891742e9cb500af26ce
Louis <louis.rivera.se@gmail.com>
2021-08-09 17:32:35 -0500

    Make initial API call
    In order to get weather data, the openweathermap API was chosen to retrieve the data. This code sets up the call to the API to retrieve the data for the three required cities.

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,17 @@
+require('dotenv').config()
+
+const fetch = require('node-fetch')
+
 const express = require('express')
 const app = express()
 
+const APIKey = process.env.WEATHER_API_KEY
 
+const chicagoWeatherID = 4887398
+const fortMyersWeatherID = 4155995
+const siouxFallsWeatherID = 5231851
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?id=${fortMyersWeatherID}&appid=${APIKey}`)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(error => console.log(`Error Found: ${error}`))
