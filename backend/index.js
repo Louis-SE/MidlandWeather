0242dc40e7105ad0fd35a8de4043469e9851c0e5
Louis <louis.rivera.se@gmail.com>
2021-08-09 19:02:48 -0500

    Implement fetching weather data
    The code now makes calls to the weather API to request data for Chicago, Fort Myer, and Sioux Falls.

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,30 @@ const chicagoWeatherID = 4887398
 const fortMyersWeatherID = 4155995
 const siouxFallsWeatherID = 5231851
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?id=${fortMyersWeatherID}&appid=${APIKey}`)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(error => console.log(`Error Found: ${error}`))
+
+const fetchWeatherData = async (locationID) => {
+    try {
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=${locationID}&units=imperial&appid=${APIKey}`)
+        const json = await res.json()
+        return json
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+
+(async () => {
+    const chicagoWeather = await fetchWeatherData(chicagoWeatherID)
+    console.log(`Chicago: ${chicagoWeather.main.temp} \u00B0F ${chicagoWeather.weather[0].description}`)
+})();
+
+(async () => {
+    const fortMyersWeather = await fetchWeatherData(fortMyersWeatherID)
+    console.log(`Fort Myer: ${fortMyersWeather.main.temp} \u00B0F ${fortMyersWeather.weather[0].description}`)
+})();
+
+(async () => {
+    const siouxFallsWeather = await fetchWeatherData(siouxFallsWeatherID)
+    console.log(`Sioux Falls: ${siouxFallsWeather.main.temp} \u00B0F ${siouxFallsWeather.weather[0].description}`)
+})();
