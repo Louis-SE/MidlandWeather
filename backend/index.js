1db72b82c6b1ca37823a363f9fe2c1991d3d789e
Louis <louis.rivera.se@gmail.com>
2021-08-09 19:43:09 -0500

    Switch to using a map for location IDs
    Instead of storing the individual weather location ID's in their own variables, one map container was created and then each of the locations now maps to their IDs. This will make it easier to add more locations in the future.

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
-
-const fetch = require('node-fetch')
-
 const express = require('express')
 const app = express()
 
+// These are required for making the HTTP call to the weather API.
+// Dotenv is creating prcoess environment variables so that the weather API key can be stored in a seperate .env file.
+require('dotenv').config()
+const fetch = require('node-fetch')
 const APIKey = process.env.WEATHER_API_KEY
 
-const chicagoWeatherID = 4887398
-const fortMyersWeatherID = 4155995
-const siouxFallsWeatherID = 5231851
+const locationIDMap = require('./locationMap')
+
+
 
 
 const fetchWeatherData = async (locationID) => {
@@ -25,16 +25,16 @@ const fetchWeatherData = async (locationID) => {
 
 
 (async () => {
-    const chicagoWeather = await fetchWeatherData(chicagoWeatherID)
+    const chicagoWeather = await fetchWeatherData(locationIDMap.get('chicago'))
     console.log(`Chicago: ${chicagoWeather.main.temp} \u00B0F ${chicagoWeather.weather[0].description}`)
 })();
 
 (async () => {
-    const fortMyersWeather = await fetchWeatherData(fortMyersWeatherID)
-    console.log(`Fort Myer: ${fortMyersWeather.main.temp} \u00B0F ${fortMyersWeather.weather[0].description}`)
+    const fortMyersWeather = await fetchWeatherData(locationIDMap.get('fort myers'))
+    console.log(`Fort Myer: ${fortMyersWeather.main.temp} \u00B0F ${fortMyersWeather .weather[0].description}`)
 })();
 
 (async () => {
-    const siouxFallsWeather = await fetchWeatherData(siouxFallsWeatherID)
+    const siouxFallsWeather = await fetchWeatherData(locationIDMap.get('sioux falls'))
     console.log(`Sioux Falls: ${siouxFallsWeather.main.temp} \u00B0F ${siouxFallsWeather.weather[0].description}`)
 })();
---
+++ backend/locationMap.js
@@ -0,0 +1,7 @@
+const locationIDMap = new Map();
+
+locationIDMap.set('chicago', 4887398)
+locationIDMap.set('fort myers', 4155995)
+locationIDMap.set('sioux falls', 5231851)
+
+module.exports = locationIDMap
