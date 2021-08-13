90f925721a53764394e842c93490d0379563bdd8
Louis <louis.rivera.se@gmail.com>
2021-08-12 19:10:08 -0500

    Setup a default get route for heroku

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ require('dotenv').config()
 const weather = require('./routes/weather')
 app.use('/api/weather', weather)
 
+app.get('*', (req, res) => {
+    res.status(200).json({Status: "Successful!"})
+})
+
 
 app.listen(port,() =>{
     console.log(`Server started at ${port}`)
